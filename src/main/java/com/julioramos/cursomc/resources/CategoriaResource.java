d8c1f538ae52ecf8fc2ff5f2dee3d170e61a7fa2
Júlio Ramos <ps2.ramos@gmail.com>
2020-04-26 17:14:38 -0300

    Implementação da primeira classe de domínio
    Motivo: I (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Teste e implementação da classe de domínio Cateroria.
    
    Observações:
    
    Referência:

--- src/main/java/com/julioramos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/julioramos/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.julioramos.cursomc.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.julioramos.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "REST funcionando";
+	public List<Categoria> listar() {
+
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+
+		List<Categoria> cats = Arrays.asList(c1, c2);
+
+		return cats;
 	}
 }
