ab41acd5ce91f2ca4dfd358f79d45dc5c26ebe1b
Júlio César Ramos <ps2.ramos@gmail.com>
2020-05-11 22:52:35 -0300

    Deleção de Categorias com o método Delete
    Motivo: I/M (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Adicionado endpoint para deleção de Categorias utilizando o método
      Http DELETE.
      - Criada nova classe para tratar exceptions de integridade pois caso
      a Categoria esteja referênciada por algum produto, a deleção não é
      permitida.
    
    Observações:
    
    Referência:

--- src/main/java/com/julioramos/cursomc/services/CategoriaService.java
+++ src/main/java/com/julioramos/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.julioramos.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.julioramos.cursomc.domain.Categoria;
 import com.julioramos.cursomc.repositories.CategoriaRepository;
+import com.julioramos.cursomc.services.exceptions.DataIntegrityException;
 import com.julioramos.cursomc.services.exceptions.ObjectNotFoundException;
 
 /**
@@ -46,5 +48,21 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	/**
+	 * Deleta uma Categoria com base no Id informado.
+	 * 
+	 * @param id Identificador da Categoria que será deleteada.
+	 */
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException ex) {
+			throw new DataIntegrityException(
+					"Não foi possível excluir a Categoria com id: " + id + " pois a mesma está sendo referênciada por outras entidades", ex);
+		}
+
 	}
 }
